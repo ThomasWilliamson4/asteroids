331240135fb9eaa4c88f928eddb85cb93fd7d772
BvWaffles <tomwilliamson4@gmail.com>
2025-01-21 20:18:44 +1030

    0.02 updated to closable window

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
     print("Starting asteroids!")
