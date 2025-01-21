2a7c57e76fde597a2a4378d07644080279f9eb5a
BvWaffles <tomwilliamson4@gmail.com>
2025-01-21 21:44:16 +1030

    0.04 able to move around the screen

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import pygame
 import constants
+import player
 from constants import *
 
 def main():
     pygame.init()
     game_clock = pygame.time.Clock()
     dt = 0
+    player_instance = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        
         pygame.Surface.fill(screen, (0, 0, 0))
+        player_instance.draw(screen)
+        player_instance.update(dt)
         pygame.display.flip()
         dt = game_clock.tick(60) /1000
     print("Starting asteroids!")
