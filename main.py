40187506617f571b708431697bd15809358479c9
BvWaffles <tomwilliamson4@gmail.com>
2025-01-21 22:19:20 +1030

    0.05 added update and drawable groups

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
 import pygame
 import constants
-import player
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     game_clock = pygame.time.Clock()
     dt = 0
-    player_instance = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    player_instance = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
+        #pygame.Surface.fill(screen, (0, 0, 0))
+        #player_instance.draw(screen)
+        #player_instance.update(dt)
+        
+        for obj in updatable:
+            obj.update(dt)
+        
         pygame.Surface.fill(screen, (0, 0, 0))
-        player_instance.draw(screen)
-        player_instance.update(dt)
+        
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         dt = game_clock.tick(60) /1000
-    print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
 
 if __name__ == "__main__":
     main()
