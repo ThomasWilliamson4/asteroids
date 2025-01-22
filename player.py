4916a9164c0e19f4c3c85d58f52373eea59bc8f9
BvWaffles <tomwilliamson4@gmail.com>
2025-01-22 11:44:06 +1030

    0.11 added shot speed

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(circleshape.CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, constants.PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0
     def draw(self, screen):
         pygame.draw.polygon(screen, (255, 255, 255), self.triangle(), 2)
 
@@ -35,12 +36,19 @@ class Player(circleshape.CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt) 
         if keys[pygame.K_SPACE]:
-            self.shoot()   
+            self.shoot()
+        if self.shoot_timer > 0:
+            self.shoot_timer -= dt
+       
     
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * constants.PLAYER_SPEED * dt
     
     def shoot(self):
-        shot = Shot(self.position.x, self.position.y)
-        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation)*constants.PLAYER_SHOOT_SPEED
+        if self.shoot_timer > 0:
+            return
+        else:
+            self.shoot_timer = constants.PLAYER_SHOOT_COOLDOWN
+            shot = Shot(self.position.x, self.position.y)
+            shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation)*constants.PLAYER_SHOOT_SPEED
